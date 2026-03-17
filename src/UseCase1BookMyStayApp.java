ec4c3e03d424b7bfdbe69765c1bbed5fe5970c41
amitojsingh1306-prog <amitojsingh1306@gmail.com>
2026-03-17 14:43:06 +0530

    Initial commit with all use cases

---
+++ src/UseCase1BookMyStayApp.java
@@ -0,0 +1,27 @@
+/**
+ * ============================================================
+ * MAIN CLASS - UseCase1BookMyStayApp
+ * ============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Book My Stay Application.
+ *
+ * It demonstrates program startup and console output.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+public class UseCase1BookMyStayApp {
+
+    /**
+     * Application entry point
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Book My Stay App v1.0");
+        System.out.println("Welcome to Book My Stay App!");
+    }
+}
