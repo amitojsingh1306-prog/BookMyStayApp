baff575b5d8a1fedd008844aca572401d026a613
amitojsingh1306-prog <amitojsingh1306@gmail.com>
2026-03-17 15:56:15 +0530

    Add UseCase5BookMyStayApp and update main file for UseCase5

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -19,8 +19,9 @@ public class BookMyStayApp {
         switch (choice) {
             //case 1 -> UseCase1BookMyStayApp.execute();
             //case 2 -> UseCase2BookMyStayApp.execute();
-            case 3 -> UseCase3BookMyStayApp.execute();
+            //case 3 -> UseCase3BookMyStayApp.execute();
             case 4 -> UseCase4BookMyStayApp.execute();
+            case 5 -> UseCase5BookMyStayApp.execute();
             default -> System.out.println("Invalid choice! Please enter a number between 1 and 7.");
         }
 
